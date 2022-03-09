0c389c0a7081dd8e06d39430d9d83a0a786c1452
Jongwoo Lee <jongwoo.developer@gmail.com>
2022-03-09 19:07:38 +0900

    Update LoginView

--- iosStudia/LoginView.swift
+++ iosStudia/LoginView.swift
@@ -58,95 +58,99 @@ struct LoginView: View {
     }
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            Text("studia")
-                .font(Font.custom("Courgette Regular", size: 40))
-                .foregroundColor(Color("ThemeColor"))
-            
-            Text("시작하기")
-                .font(Font.custom("NanumSquare_ac Bold", size: 50))
-            
-            //
-            
-            Button(action: {
-                /*Auth.auth().signIn(withEmail: email, password: password) {(user, error)in
-                    if(user != nil) {
-                        // Logged in
-                        self.isLoggedIn = true
-                    } else {
-                        // Login failed, show alert
-                        self.loginFailed = true
-                    }
-                }*/
-            }) {
-                Text("로그인")
-                    .font(.system(size: 15, weight: .medium))
-                    .frame(minWidth: 0, maxWidth: .infinity)
-                    .frame(height: 40, alignment: .center)
-            }
-            .background(Color("ThemeColor"))
-            .foregroundColor(Color.white)
-            .cornerRadius(10)
-            
-            Button(action: {
-                guard let clientID = FirebaseApp.app()?.options.clientID else { return }
-                let config = GIDConfiguration(clientID: clientID)
+        NavigationView {
+            VStack(alignment: .leading, spacing: 0) {
+                Text("studia")
+                    .font(Font.custom("Courgette Regular", size: 40))
+                    .foregroundColor(Color("ThemeColor"))
                 
-                // Start Signin flow
-                GIDSignIn.sharedInstance.signIn(with: config, presenting: (UIApplication.shared.windows.first?.rootViewController)!) { user, error in
-                    if error != nil { return }
+                Text("시작하기")
+                    .font(Font.custom("NanumSquare_ac Bold", size: 50))
+                    .padding(.bottom, 50)
+                
+                //
+                /*Auth.auth().signIn(withEmail: email, password: password) {(user, error)in
+                 if(user != nil) {
+                 // Logged in
+                 self.isLoggedIn = true
+                 } else {
+                 // Login failed, show alert
+                 self.loginFailed = true
+                 }
+                 }*/
+                
+                NavigationLink(destination: PWLoginView()) {
+                    Label("이메일, 비밀번호로 로그인", systemImage: "key.fill")
+                        .font(.system(size: 17, weight: .medium))
+                }
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .frame(height: 45, alignment: .center)
+                .background(Color("LoginBtnColor"))
+                .foregroundColor(Color.white)
+                .cornerRadius(10)
+                
+                Button(action: {
+                    guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+                    let config = GIDConfiguration(clientID: clientID)
                     
-                    guard
-                        let authentication = user?.authentication,
-                        let idToken = authentication.idToken
-                    else { return }
-                    
-                    let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: authentication.accessToken)
-                    
-                    // Firebase Auth with Credential
-                    Auth.auth().signIn(with: credential) { (authResult, error) in
+                    // Start Signin flow
+                    GIDSignIn.sharedInstance.signIn(with: config, presenting: (UIApplication.shared.windows.first?.rootViewController)!) { user, error in
                         if error != nil { return }
                         
-                        // Signed in successfully
-                        isLoggedIn = true
+                        guard
+                            let authentication = user?.authentication,
+                            let idToken = authentication.idToken
+                        else { return }
+                        
+                        let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: authentication.accessToken)
+                        
+                        // Firebase Auth with Credential
+                        Auth.auth().signIn(with: credential) { (authResult, error) in
+                            if error != nil { return }
+                            
+                            // Signed in successfully
+                            isLoggedIn = true
+                        }
+                    }
+                }) {
+                    Label{
+                        Text("Google로 로그인")
+                            .font(.system(size: 17, weight: .medium))
+                    } icon: {
+                        Image("google_login")
+                            .resizable()
+                            .frame(width: 17, height: 17)
                     }
                 }
-            }) {
-                Image("google_login")
-                    .resizable()
-                    .frame(width: 20, height: 20)
-                
-                Text("Google 계정으로 로그인")
-                    .font(.system(size: 15, weight: .medium))
-            }
-            .frame(minWidth: 0, maxWidth: .infinity)
-            .frame(height: 40, alignment: .center)
-            .background(Color("BgColor"))
-            .cornerRadius(10)
-            .foregroundColor(Color("TextColor"))
-            .overlay(
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(Color("ThemeColor"), lineWidth: 2)
-            )
-            .padding(.top, 5)
-            
-            SignInWithAppleButton(.signIn, onRequest: AppleIDSignInConfigure, onCompletion: AppleIDSignInHandle)
-                .signInWithAppleButtonStyle(
-                    colorScheme == .dark ? .white : .black
-                )
                 .frame(minWidth: 0, maxWidth: .infinity)
-                .frame(height: 40, alignment: .center)
+                .frame(height: 45, alignment: .center)
+                .background(Color("BgColor"))
                 .cornerRadius(10)
+                .foregroundColor(Color("TextColor"))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color("ThemeColor"), lineWidth: 2)
+                )
                 .padding(.top, 5)
-        }
-        .frame(minWidth: 0, maxWidth: 500)
-        .padding()
-        .navigate(to: CalendarView(), when: $isLoggedIn)
-        .alert(isPresented: $loginFailed) {
-            Alert(title: Text("로그인 실패"),
-                  message: Text("가입되지 않은 아이디이거나 비밀번호가 잘못되었습니다."),
-                  dismissButton: .default(Text("확인"))
-            )
+                
+                SignInWithAppleButton(.signIn, onRequest: AppleIDSignInConfigure, onCompletion: AppleIDSignInHandle)
+                    .signInWithAppleButtonStyle(
+                        colorScheme == .dark ? .white : .black
+                    )
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .frame(height: 45, alignment: .center)
+                    .cornerRadius(10)
+                    .padding(.top, 5)
+            }
+            .frame(minWidth: 0, maxWidth: 500)
+            .padding()
+            .navigationBarHidden(true)
+            .alert(isPresented: $loginFailed) {
+                Alert(title: Text("로그인 실패"),
+                      message: Text("가입되지 않은 아이디이거나 비밀번호가 잘못되었습니다."),
+                      dismissButton: .default(Text("확인"))
+                )
+            }
         }
     }
     
@@ -220,64 +224,70 @@ struct PWLoginView: View {
     @State private var password: String = ""        // User input for password
     
     var body: some View {
-        Text("이메일 주소")
-            .font(.caption)
-            .padding(.leading, 15)
-        
-        HStack {
-            TextField("email@example.com", text: $email)
-                .disableAutocorrection(true)
-                .keyboardType(.emailAddress)
-                .autocapitalization(.none)
+        VStack(alignment: .leading) {
+            Text("이메일 주소")
+                .font(.caption)
                 .padding(.leading, 15)
-        }
-        .frame(height: 40)
-        .background(Color("TextFieldBgColor"))
-        .cornerRadius(20)
-        .padding(.bottom, 10)
-        
-        Text("비밀번호")
-            .font(.caption)
-            .padding(.leading, 15)
-        
-        HStack {
-            SecureField("• • • • • • • •", text: $password)
-                .disableAutocorrection(true)
-                .autocapitalization(.none)
+            
+            HStack {
+                TextField("email@example.com", text: $email)
+                    .disableAutocorrection(true)
+                    .keyboardType(.emailAddress)
+                    .autocapitalization(.none)
+                    .padding(.leading, 15)
+            }
+            .frame(height: 40)
+            .background(Color("TextFieldBgColor"))
+            .cornerRadius(20)
+            .padding(.bottom, 10)
+            
+            Text("비밀번호")
+                .font(.caption)
                 .padding(.leading, 15)
-        }
-        .frame(height: 40)
-        .background(Color("TextFieldBgColor"))
-        .cornerRadius(20)
-        .padding(.bottom, 10)
-        
-        HStack {
-            Button(action: {
-                //
-            }) {
-                Text("계정 만들기")
-                    .font(.body)
-                    .foregroundColor(Color.black)
-            }
-            .frame(height: 30)
             
-            Spacer()
-            
-            Button(action: {
-                //
-            }) {
-                Text("비밀번호 찾기")
-                    .font(.body)
-                    .foregroundColor(Color.black)
+            HStack {
+                SecureField("• • • • • • • •", text: $password)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .padding(.leading, 15)
             }
-            .frame(height: 30)
+            .frame(height: 40)
+            .background(Color("TextFieldBgColor"))
+            .cornerRadius(20)
+            .padding(.bottom, 10)
+            
+            HStack {
+                Button(action: {
+                    //
+                }) {
+                    Text("계정 만들기")
+                        .font(.body)
+                        .foregroundColor(Color.black)
+                }
+                .frame(height: 30)
+                
+                Spacer()
+                
+                Button(action: {
+                    //
+                }) {
+                    Text("비밀번호 찾기")
+                        .font(.body)
+                        .foregroundColor(Color.black)
+                }
+                .frame(height: 30)
+            }
+            .padding(10)
         }
-        .padding(10)
+        .padding()
+        .navigationTitle("로그인")
+        .navigationBarTitleDisplayMode(.large)
     }
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
+        NavigationView { PWLoginView() }
     }
 }
